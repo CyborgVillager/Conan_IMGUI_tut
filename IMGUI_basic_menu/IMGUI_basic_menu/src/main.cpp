993655909d6ab5f4c3f323162ec705c1727c182c
CyborgVillager <almawijonathan@gmail.com>
2024-01-13 05:57:34 -0600

    v2
    v2

--- IMGUI_basic_menu/IMGUI_basic_menu/src/main.cpp
+++ IMGUI_basic_menu/IMGUI_basic_menu/src/main.cpp
@@ -67,7 +67,7 @@ int main(int, char**)
     //ImGui_ImplWin32_EnableDpiAwareness();
     WNDCLASSEXW wc = { sizeof(wc), CS_CLASSDC, WndProc, 0L, 0L, GetModuleHandle(nullptr), nullptr, nullptr, nullptr, nullptr, L"ImGui Example", nullptr };
     ::RegisterClassExW(&wc);
-    HWND hwnd = ::CreateWindowW(wc.lpszClassName, L" exmaple2 Example", WS_OVERLAPPEDWINDOW, 100, 100, 1280, 800, nullptr, nullptr, wc.hInstance, nullptr);
+    HWND hwnd = ::CreateWindowW(wc.lpszClassName, L" IMGUI_basic_menu_5_57_AM", WS_OVERLAPPEDWINDOW, 100, 100, 1280, 800, nullptr, nullptr, wc.hInstance, nullptr);
 
     // Initialize Direct3D
     if (!CreateDeviceD3D(hwnd))
