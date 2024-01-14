c12964a6a397ebc2b8aae23e7dad9f64b9b1cc56
CyborgVillager <almawijonathan@gmail.com>
2024-01-14 05:54:28 -0600

    THIS PUSH : Dear ImGui Tutorials C++ (#3) Checkboxes, Buttons, Sliders
    THIS PUSH : Dear ImGui Tutorials C++ (#3) Checkboxes, Buttons, Sliders
    https://youtu.be/WwgU7am-9Cs?si=6aaG_Q_5l_pN2h3W

--- IMGUI_basic_menu/IMGUI_basic_menu/src/main.cpp
+++ IMGUI_basic_menu/IMGUI_basic_menu/src/main.cpp
@@ -119,6 +119,12 @@ int main(int, char**)
     bool show_demo_window = true;
     bool show_another_window = false;
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+    //CheckBox
+    bool test_bool_checkbox = false;
+    //Slider
+    int int_test_slider = 0;
+    float float_test = 0.0f;
+
 
     // Main loop
     bool done = false;
@@ -141,43 +147,41 @@ int main(int, char**)
         ImGui_ImplDX12_NewFrame();
         ImGui_ImplWin32_NewFrame();
         ImGui::NewFrame();
-
-        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-        if (show_demo_window)
-            ImGui::ShowDemoWindow(&show_demo_window);
-
-        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
+        //Dear ImGui Tutorials C++ (#2) Window Flags, SetSize, Basics -Skyed
+        //https://youtu.be/GEJmTAmBZ8A?si=SiH6gsiriG5hPPWr
+        // SunVox Compo 2023.02 Winners
+        // https://youtu.be/2JTwUsBKgBU?si=wa7FnpkmbRzKI15y
+        //Create Window Here
+        //THIS PUSH : Dear ImGui Tutorials C++ (#3) Checkboxes, Buttons, Sliders
+        //Window Size
+        ImGui::SetNextWindowSize(ImVec2(500, 500));
+        //Window Tab Box
+        if (ImGui::Begin("Window Test", NULL,ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse))
         {
-            static float f = 0.0f;
-            static int counter = 0;
+            //Checkbox
+            ImGui::Checkbox("Test Bool-Checkbox", &test_bool_checkbox);
+            //Button -  to open a test_bool_checkbox new window , it connects to      if (test_bool_checkbox == true)
+            if (ImGui::Button("Click me!"))
+            {
+                test_bool_checkbox = true;
+            }
+            //Slider  , min slider is 0, max slider value is 25
+           ImGui::SliderInt("Choose Number", &int_test_slider,0, 25);
+            ImGui::SliderFloat("Choose Float", &float_test, 0.0f, 25.5f);
 
-            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
 
-            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-            ImGui::Checkbox("Another Window", &show_another_window);
+        }ImGui::End();
+       
+        //Button to open a test_bool_checkbox new window int_test_slider == 25
+        if (test_bool_checkbox == true || int_test_slider == 25 || float_test == 25.5f)
+        {
+            ImGui::SetNextWindowSize(ImVec2(200, 200));
+            if (ImGui::Begin("window 2"))
+            {
 
-            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-                counter++;
-            ImGui::SameLine();
-            ImGui::Text("counter = %d", counter);
-
-            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
-            ImGui::End();
+            }ImGui::End();
         }
 
-        // 3. Show another simple window.
-        if (show_another_window)
-        {
-            ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-            ImGui::Text("Hello from another window!");
-            if (ImGui::Button("Close Me"))
-                show_another_window = false;
-            ImGui::End();
-        }
 
         // Rendering
         ImGui::Render();
